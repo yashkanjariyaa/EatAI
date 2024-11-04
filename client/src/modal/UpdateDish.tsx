d7c571fcad816fc95fee84523f6ae9c68b4bbb35
yashkanjariyaa <yashkanjariyaff@gmail.com>
2024-11-04 14:24:05 +0530

    deployment errors

--- client/src/modal/UpdateDish.tsx
+++ client/src/modal/UpdateDish.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./UpdateDish.css";
+import "./updatedish.css";
 
 interface UpdateDishProps {
   isOpen: boolean;
